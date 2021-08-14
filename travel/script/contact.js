ec77cc292b6db6260b8fdb1d34ccad27d6264c31
raashitha <88303289+raashitha@users.noreply.github.com>
2021-08-14 20:07:24 +0530

    Update contact.js

--- travel/script/contact.js
+++ travel/script/contact.js
@@ -2,16 +2,16 @@ function validate() {
   var destination = document.getElementById("destination").value;
   var paymentt= document.getElementById("fname").value;
  
-  if(paymentt.length === 12 )
+   if(destination === '')
+  {
+    document.getElementById('error').innerHTML="destination should be filled...";
+    return false;
+  }else if(paymentt.length === 12 )
   {
     document.getElementById('error').innerHTML="payment accepted";
     window.alter("payment accepted");
     return true;
     
-  }else if(destination === '')
-  {
-    document.getElementById('error').innerHTML="destination should be filled...";
-    return false;
   }
   else{  
      document.getElementById('error').innerHTML="invalid payment number...";
